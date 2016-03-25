493b317457d374c9aa03d73f0a3ea2b0bbc5c884
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-03-25 12:56:14 -0500

    Serves all files in media directory as static files

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
 var app = express();
 
+app.use('/', express.static('media'));
+
 module.exports = app;
--- test/media.js
+++ test/media.js
@@ -1,6 +1,7 @@
 var request = require('supertest');
 var express = require('express');
-
+var path = require('path');
+var fs = require('fs');
 var app = require('../server');
 
 context('Given there are no files in the media directory', function(){
@@ -8,5 +9,23 @@ context('Given there are no files in the media directory', function(){
     request(app)
       .get('/foo')
       .expect(404, done);
+  });
+});
+
+context('Given there is a single file in the media directory', function(){
+  var fooFile = path.join(__dirname, '..', 'media', 'foo');
+
+  before(function(){
+    fs.writeFile(fooFile, 'bar');
+  });
+
+  after(function(){
+    fs.unlink(fooFile);
+  });
+
+  it('will respond with a 200', function(done){
+    request(app)
+      .get('/foo')
+      .expect(200, done);
   });
 });
