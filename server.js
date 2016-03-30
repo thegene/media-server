953fe8a43b86136025879f07235b9c3ec1359d4c
Eugene Westbrook <eugenewestbrook@gmail.com>
2016-03-30 12:57:31 -0500

    Gives a little more output on what is being served

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 var path = require('path');
-
+var fs = require('fs');
 var express = require('express');
 var app = express();
 
 var mediaDirectory = process.env['MEDIA_DIRECTORY'] || path.join(__dirname, 'media');
+fs.readdir(mediaDirectory, function(error, files) {
+  console.log('Found the following media: ' + files.toString());
+});
 
 console.log('Serving media from ' + mediaDirectory);
 
 app.use('/', express.static(mediaDirectory));
+
+app.use(function(error, request, response, next){
+  console.error(error.stack);
+  response.send(500).send(error.stack);
+});
 
 app.listen(3000);
 
